9f49513850ee1e3a3dfc141eee46619aa0783048
kapoor100 <kapoorsookoo123@gmail.com>
2024-01-04 00:31:02 -0600

    working on api calls

--- nextjs-dashboard/app/items/route.ts
+++ nextjs-dashboard/app/items/route.ts
@@ -1,2 +1,25 @@
 export const dynamic = 'force-dynamic' // defaults to auto
-export async function GET(request: Request) {}
+export async function GET() {
+    const res = await fetch('https://data.mongodb-api.com/...', {
+      next: { revalidate: 60 }, // Revalidate every 60 seconds
+    })
+    const data = await res.json()
+   
+    return Response.json(data)
+  }
+  export async function POST() {
+    const res = await fetch('https://data.mongodb-api.com/...', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'API-Key': process.env.DATA_API_KEY!,
+      },
+      body: JSON.stringify({ time: new Date().toISOString() }),
+    })
+   
+    const data = await res.json()
+   
+    return Response.json(data)
+  }
+  
+
